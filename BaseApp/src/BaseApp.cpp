01f80e0bf2f51c3d14948f5d8ba57b8f69db06a0
Ezekiel Kigbo <theatlasroom@gmail.com>
2013-05-26 04:55:28 +1000

    added x

--- BaseApp/src/BaseApp.cpp
+++ BaseApp/src/BaseApp.cpp
@@ -29,6 +29,9 @@ void BaseApp::draw()
 {
 	// clear out the window with black
 	gl::clear(Color(0.0f, 0.0f, 0.0f)); 
+	//draw a x over thew whole window area
+	gl::drawLine(Vec2f(0.0f,0.0f), Vec2f(getWindowWidth(), getWindowHeight()));
+	gl::drawLine(Vec2f(0.0f,getWindowHeight()), Vec2f(getWindowWidth(), 0.0f));
 }
 
 CINDER_APP_BASIC( BaseApp, RendererGl )
